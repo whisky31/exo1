3757df2ae1c18e0b9046f4125329774f86f8b781
whisky31 <103508837+whisky31@users.noreply.github.com>
2022-04-12 14:55:32 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+let n = 0
+while (!(input.buttonIsPressed(Button.B))) {
+    if (true) {
+    	
+    }
+}
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(25)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(45)
 })
